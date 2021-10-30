01e20c50b919186360fca17e6dd9e33a68af11fd
unknown <bekir.akok@hotmail.com>
2021-10-30 19:53:12 +0300

    Readme.md uptated && searchbar component edited.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { searchItems } from '../../redux/actions/action';
 import { ThemeContext } from "../../context/ThemeContext";
+import { RiSearch2Line, RiSearchEyeFill } from 'react-icons/ri';
 import './searchBar.scss';
 
 const SearchBar = () => {
@@ -31,6 +32,15 @@ const SearchBar = () => {
                 value={search}
                 style={{ border: `${darkMode ? "2px solid #f5f5f5" : " 1px solid #4c4b4e"}` }}
                 onChange={(e) => setSearch(e.target.value)} />
+            {
+                darkMode
+                    ? <span>
+                        <RiSearchEyeFill />
+                    </span>
+                    : <span>
+                        <RiSearch2Line />
+                    </span>
+            }
         </div>
     )
 }
