a728bc1240f6bacd2ee26dcea820d1cef79cdf90
Bekir Akok <bekir.akok@ekinokssoftware.com>
2021-11-29 21:07:18 +0300

    kodluyoruz github

--- src/components/Closer/Closer.js
+++ src/components/Closer/Closer.js
@@ -1,35 +1,33 @@
-import React from 'react';
-import { GrClose } from 'react-icons/gr';
-import { useDispatch } from 'react-redux';
-import { removeToFav, removeToCatch } from '@redux/actions/action';
-import { useParams } from 'react-router-dom';
-import './closer.scss';
+import React from "react";
+import { GrClose } from "react-icons/gr";
+import { useDispatch } from "react-redux";
+import { removeToFav, removeToCatch } from "@redux/actions/action";
+import { useParams } from "react-router-dom";
+import "./closer.scss";
 
 const Closer = ({ pokemon, closer }) => {
+  const dispatch = useDispatch();
+  const params = useParams();
 
-    const dispatch = useDispatch();
-    const params = useParams();
+  const clickHandler = (pokemon) => {
+    params.name === "fav"
+      ? dispatch(removeToFav(pokemon))
+      : dispatch(removeToCatch(pokemon));
+  };
+  //kodluyoruz
+  return (
+    <>
+      {closer ? (
+        <div
+          className="closer"
+          title={`Remove to ${params.name === "fav" ? "Favorites" : "Catches"}`}
+          onClick={() => clickHandler(pokemon)}
+        >
+          <GrClose />
+        </div>
+      ) : null}
+    </>
+  );
+};
 
-    const clickHandler = (pokemon) => {
-        params.name === "fav"
-            ? dispatch(removeToFav(pokemon))
-            : dispatch(removeToCatch(pokemon))
-    }
-//dsömdsmdömsa
-    return (
-        <>
-            {
-                closer
-                    ? <div
-                        className="closer"
-                        title={`Remove to ${params.name === "fav" ? 'Favorites' : 'Catches'}`}
-                        onClick={() => clickHandler(pokemon)}>
-                        <GrClose />
-                    </div>
-                    : null
-            }
-        </>
-    )
-}
-
-export default Closer
+export default Closer;
